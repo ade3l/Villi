a4c18fc8c6297c3e8f379b048e7c9738bbe4dc25
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-11 18:41:19 +0300

    Created the widget

---
+++ app/src/main/java/com/example/hciproject/widgetProvider.java
@@ -0,0 +1,13 @@
+package com.example.hciproject;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class widgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+    }
+}
