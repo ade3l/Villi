8ca8d752e9df0eb2034e9e05766ab0d3a5e88411
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-12 15:01:17 +0300

    Sample bottom nav added

--- app/src/main/java/com/example/hciproject/MainActivity.java
+++ app/src/main/java/com/example/hciproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hciproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.gauravk.bubblenavigation.IBubbleNavigation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        finish();
+
+
     }
 }
