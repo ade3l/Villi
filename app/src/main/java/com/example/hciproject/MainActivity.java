d5504e3662571029af6a599b2e16756d3302359e
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-12 18:31:55 +0300

    Comments

--- app/src/main/java/com/example/hciproject/MainActivity.java
+++ app/src/main/java/com/example/hciproject/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.hciproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-import com.gauravk.bubblenavigation.IBubbleNavigation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        finish();
-
 
     }
 }
