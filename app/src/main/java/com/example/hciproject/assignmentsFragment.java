d0981996ff69444c883054bd4697a8fb357ed689
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-13 21:10:04 +0300

    Changes

--- app/src/main/java/com/example/hciproject/assignmentsFragment.java
+++ app/src/main/java/com/example/hciproject/assignmentsFragment.java
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class assignmentsFragment extends Fragment {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class assignmentsFragment extends Fragment {
+    static FloatingActionButton fab;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,16 @@ public class assignmentsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_assignments, container, false);
+        View inflatedView=inflater.inflate(R.layout.fragment_assignments, container, false);
+        fab=inflatedView.findViewById(R.id.add_task);
+        fab.show();
+
+        return inflatedView;
+    }
+    static void hideFab(){
+        fab.hide();
+    }
+    static void showFab(){
+        fab.show();
     }
 }
