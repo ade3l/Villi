f6f0af57777d469f9a72e0479934dd4ee0521a14
Adeel <71775487+ade3l@users.noreply.github.com>
2021-11-03 11:45:30 +0300

    Add class toolbar added

--- app/src/main/java/com/example/hciproject/addClassActivity.java
+++ app/src/main/java/com/example/hciproject/addClassActivity.java
@@ -1,6 +1,7 @@
 package com.example.hciproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,10 @@ public class addClassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_class);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Add a class");
+
     }
 }
