774b26584a516356e75f105178f57f5c5ac3634f
Adeel <71775487+ade3l@users.noreply.github.com>
2021-11-03 10:34:58 +0300

    add Class button

--- app/src/main/java/com/example/hciproject/timetableFragment.java
+++ app/src/main/java/com/example/hciproject/timetableFragment.java
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class timetableFragment extends Fragment {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class timetableFragment extends Fragment {
+    FloatingActionButton addClassButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +20,18 @@ public class timetableFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timetable, container, false);
+        View view=inflater.inflate(R.layout.fragment_timetable, container, false);
+        addClassButton =view.findViewById(R.id.add_class);
+        addClassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addClass();
+            }
+        });
+        return view;
+    }
+
+    void addClass(){
+        
     }
 }
