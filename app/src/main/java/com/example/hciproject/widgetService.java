10188c8beee2915fa463a7d2e3cf0ab9226acfff
Adeel <71775487+ade3l@users.noreply.github.com>
2021-11-10 21:25:47 +0300

    Made widget show subject and time

--- app/src/main/java/com/example/hciproject/widgetService.java
+++ app/src/main/java/com/example/hciproject/widgetService.java
@@ -8,8 +8,13 @@ import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
+import com.example.hciproject.data.DataSource;
+import com.example.hciproject.objects.Classes;
+
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -22,7 +27,7 @@ public class widgetService extends RemoteViewsService {
         fri=new Day("Friday"),
         sat=new Day("Saturday"),
         sun=new Day("Sunday");
-
+    List<Classes> listOfclasses=new ArrayList<>();
 
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
@@ -32,37 +37,18 @@ public class widgetService extends RemoteViewsService {
         private Context context;
         private int appWidgetId;
         int day;
-        private String[] classes={"CAO","DAA","DBMS","HCI","CAO","DAA","DBMS","HCI"};
         SharedPreferences prefs;
 
         widgetFactory(Context context,Intent intent){
             this.context=context;
-
             this.appWidgetId=intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,AppWidgetManager.INVALID_APPWIDGET_ID);
             prefs = context.getSharedPreferences("com.example.hciproject",Context.MODE_PRIVATE);
             day=prefs.getInt("day", 0);
-
         }
         @Override
         public void onCreate() {
             Log.i("mine","Widget Factory on create");
             day=prefs.getInt("day", 0);
-
-            mon.addToSchedule(LocalTime.of(8,0,0),"CAO");
-            mon.addToSchedule(LocalTime.of(9,0,0),"DAA");
-            mon.addToSchedule(LocalTime.of(12,0,0),"HCI");
-            mon.addToSchedule(LocalTime.of(11,0,0),"DBMS");
-
-            tue.addToSchedule(LocalTime.of(9,0,0),"DBMS");
-            tue.addToSchedule(LocalTime.of(10,0,0),"HCI");
-            tue.addToSchedule(LocalTime.of(11,0,0),"STS");
-            tue.addToSchedule(LocalTime.of(14,0,0),"OS");
-
-            wed.addToSchedule(LocalTime.of(9,0,0),"DBMS");
-            wed.addToSchedule(LocalTime.of(9,0,0),"CAO");
-            wed.addToSchedule(LocalTime.of(9,0,0),"DAA");
-
-
         }
 
         @Override
@@ -78,16 +64,8 @@ public class widgetService extends RemoteViewsService {
         @Override
         public int getCount() {
             day=prefs.getInt("day", 0);
-            switch(day){
-                case 0:
-                    return mon.getSchedule().size();
-                case 1:
-                    return tue.getSchedule().size();
-                case 2:
-                    return wed.getSchedule().size();
-
-                default: return 0;
-            }
+            listOfclasses= DataSource.getClasses(DataSource.getDays().get(day));
+            return listOfclasses.size();
         }
 
         @Override
@@ -95,22 +73,9 @@ public class widgetService extends RemoteViewsService {
             RemoteViews view=new RemoteViews(context.getPackageName(),R.layout.widget_item);
             day=prefs.getInt("day", 0);
             LocalTime x;
-            SortedMap<LocalTime,String> schedule = new TreeMap<LocalTime,String>();
-            switch(day){
-                case 0:
-                    schedule=mon.getSchedule();
-                    break;
-                case 1:
-                    schedule=tue.getSchedule();
-                    break;
-                case 2:
-                    schedule=wed.getSchedule();
-                    break;
-                default: view.setTextViewText(R.id.subjectName,classes[i]);;
-            }
-            x= (LocalTime) schedule.keySet().toArray()[i];
-            view.setTextViewText(R.id.subjectName, schedule.get(x));
-
+            Classes myClass=listOfclasses.get(i);
+            view.setTextViewText(R.id.subjectName, myClass.getSubject());
+            view.setTextViewText(R.id.time,String.format("%s - %s",myClass.getStartTime(),myClass.getEndTime()));
             return view;
         }
 
